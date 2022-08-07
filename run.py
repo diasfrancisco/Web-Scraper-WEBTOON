07ba910a96977b803dd014fac298741e0a5c98bd
CiscoUbuntu <diasfrancisco7798@yahoo.co.uk>
2022-08-07 01:00:44 +0100

    added _test.ipynb and webtoon/__pycache__ to git ignore and removed them from the repo | changed the basic info method to be asynchrounous

--- run.py
+++ run.py
@@ -8,18 +8,24 @@ def main():
     '''This is the main script file that runs the major methods in the Webtoon() class.
     Its uses a 'with' block to make sure the scraper closes once complete'''
     with Webtoon(collapse=True) as bot:
+        t0 = time.time()
         bot.get_main_page()
         bot.bypass_age_gate()
         bot.load_and_accept_cookies()
         bot.create_main_dirs()
         t1 = time.time()
         bot.scrape_genres_and_webtoon_urls()
+        print()
         print(f'Completed genre and webtoon url collection in {time.time() - t1} seconds')
         t2 = time.time()
         asyncio.run(bot.get_webtoon_info())
         print(f'Gathered all webtoon info in {time.time() - t2} seconds')
         # bot.get_IDs_and_imgs()
         # print(f'Scraped all images in {time.time() - start_time} seconds')
+        print()
+        print('------------------------------ Total time ------------------------------')
+        print(f'                       {time.time() - t0} seconds')
+        print()
 
 if __name__=="__main__":
     main()
--- webtoon/single_webtoon.py
+++ webtoon/single_webtoon.py
@@ -53,78 +53,34 @@ class GetDetails:
         }
         async with session.get(webtoon_url, headers=headers) as response:
             assert response.status == 200
-            text = await response.text()
+            html = await response.text()
+
+            dict_of_webtoon_info = {}
+            all_info = {}
+
+            soup = BeautifulSoup(html, 'lxml')
+            genre = soup.find('div', {'class': 'info'}).h2.text
+            all_info["Genre"] = genre
+            title = soup.find('div', {'class': 'info'}).h1.text
+            all_info["Title"] = title
+            try:
+                if soup.find('div', {'class': 'author_area'}).a.text and soup.find('div', {'class': 'author_area'}).text is not None:
+                    all_authors = " ".join(soup.find('div', {'class': 'author_area'}).text.split())[:-12]
+                    all_info["Authors"] = all_authors
+                else:
+                    author = soup.find('div', {'class': 'author_area'}).a.text
+                    all_info["Author"] = author
+            except Exception:
+                no_link_author = " ".join(soup.find('div', {'class': 'author_area'}).find(text=True, recursive=False).split())
+                all_info["Author"] = no_link_author
+            views = soup.find('span', {'class': 'ico_view'}).find_next_sibling('em').text
+            all_info["Views"] = views
+            subscribers = soup.find('span', {'class': 'ico_subscribe'}).find_next_sibling('em').text
+            all_info["Subscribers"] = subscribers
+            rating = soup.find('span', {'class': 'ico_grade5'}).find_next_sibling('em').text
+            all_info["Rating"] = rating
+
+            dict_of_webtoon_info[webtoon_url] = all_info
+
             with open(const.ALL_WEBTOONS_DIR_PATH + f'/{webtoon_url.split("/")[5]}/basic_info.json', 'w') as f:
-                json.dump(text, f)
-
-
-
-
-
-
-        # CreateDirs.webtoon_dir(self, webtoon_url)
-
-        # with open(const.ALL_WEBTOONS_DIR_PATH + f'/{webtoon_url.split("/")[5]}/basic_info.json', 'w') as f:
-        #     f.close()
-
-        # self.driver.get(webtoon_url)
-    
-        # dict_of_webtoon_info = {}
-        # all_info = {}
-        
-        # # Wait for the episode container to appear
-        # try:
-        #     WebDriverWait(self.driver, const.DELAY).until(
-        #         EC.presence_of_element_located(
-        #             (By.XPATH, '//*[@id="_listUl"]') and
-        #             ((By.CLASS_NAME, 'info'))
-        #         )
-        #     )
-        # except TimeoutException:
-        #     print(f"Episode and stats container did not load for {webtoon_url}")
-
-        # # Get info on genre, title and author
-        # info_container = self.driver.find_element(By.CLASS_NAME, 'info')
-        # genre = info_container.find_element(By.XPATH, '//h2')
-        # title = info_container.find_element(By.XPATH, '//h1')
-        # author_area = info_container.find_element(By.CLASS_NAME, 'author_area')
-        # all_info["Genre"] = [genre.text]
-        # all_info["Title"] = [title.text]
-        # try:
-        #     author = author_area.find_element(By.TAG_NAME, 'a')
-        #     all_info["Author"] = [author.text]
-        # except Exception:
-        #     no_link_author = info_container.find_element(By.CLASS_NAME, 'author_area')
-        #     all_info["Author"] = [no_link_author.text[:-12]]
-
-        # # Get all stat details (views, subscribers, rating)
-        # try:
-        #     WebDriverWait(self.driver, const.DELAY).until(
-        #         EC.presence_of_element_located((
-        #             By.XPATH,
-        #             '//span[@class="ico_view"]/following-sibling::em' and
-        #             '//span[@class="ico_subscribe"]/following-sibling::em' and
-        #             '//span[@class="ico_grade5"]/following-sibling::em'
-        #         ))
-        #     )
-        # except TimeoutException:
-        #     print("Stats did not load")
-
-        # stats_container = self.driver.find_element(By.CLASS_NAME, 'grade_area')
-        # views = stats_container.find_element(
-        #     By.XPATH, '//*[@class="ico_view"]/following-sibling::em'
-        # )
-        # subscribers = stats_container.find_element(
-        #     By.XPATH, '//*[@class="ico_subscribe"]/following-sibling::em'
-        # )
-        # rating = stats_container.find_element(
-        #     By.XPATH, '//*[@class="ico_grade5"]/following-sibling::em'
-        # )
-        # all_info["Views"] = [views.text]
-        # all_info["Subscribers"] = [subscribers.text]
-        # all_info["Rating"] = [rating.text]
-
-        # dict_of_webtoon_info[webtoon_url] = [all_info]
-
-        # with open(const.ALL_WEBTOONS_DIR_PATH + f'/{webtoon_url.split("/")[5]}/basic_info.json', 'w') as f:
-        #     json.dump(dict_of_webtoon_info, f)
+                json.dump(dict_of_webtoon_info, f, indent=4)
